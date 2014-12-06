9ae59baf15e2350fb3e8aaddf16b41be352b0adb
yejianjie <244085027>
2014-12-06 20:24:10 +0800

    add some

--- TripCard/.apt_generated/com/m/tripcard/ui/activity/login/LoginActivity$$ViewInjector.java
+++ TripCard/.apt_generated/com/m/tripcard/ui/activity/login/LoginActivity$$ViewInjector.java
@@ -7,16 +7,6 @@ import butterknife.ButterKnife.Finder;
 public class LoginActivity$$ViewInjector {
   public static void inject(Finder finder, final com.m.tripcard.ui.activity.login.LoginActivity target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2130968580, "field 'loginWeibo' and method 'loginByWeibo'");
-    target.loginWeibo = (android.widget.TextView) view;
-    view.setOnClickListener(
-      new android.view.View.OnClickListener() {
-        @Override public void onClick(
-          android.view.View p0
-        ) {
-          target.loginByWeibo();
-        }
-      });
     view = finder.findRequiredView(source, 2130968579, "field 'loginQQ' and method 'loginByQQ'");
     target.loginQQ = (android.widget.TextView) view;
     view.setOnClickListener(
@@ -27,10 +17,20 @@ public class LoginActivity$$ViewInjector {
           target.loginByQQ();
         }
       });
+    view = finder.findRequiredView(source, 2130968580, "field 'loginWeibo' and method 'loginByWeibo'");
+    target.loginWeibo = (android.widget.TextView) view;
+    view.setOnClickListener(
+      new android.view.View.OnClickListener() {
+        @Override public void onClick(
+          android.view.View p0
+        ) {
+          target.loginByWeibo();
+        }
+      });
   }
 
   public static void reset(com.m.tripcard.ui.activity.login.LoginActivity target) {
-    target.loginWeibo = null;
     target.loginQQ = null;
+    target.loginWeibo = null;
   }
 }
